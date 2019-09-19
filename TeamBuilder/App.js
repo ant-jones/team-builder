91f0e6f13190e8c2dd7b7080594ccb1465ef8ae8
Antoinette <create@aplusmedia.info>
2019-09-18 23:04:20 -0400

    corrections

--- TeamBuilder/App.js
+++ TeamBuilder/App.js
@@ -1,10 +1,22 @@
 import React, {useState} from "react"
+import ReactDOM from "react-dom"
+import Form from
 
-const TeamBuilder = props => {
+function App () {
+    const [team, setTeam] = useState([
+        {
+        name: "Sheri",
+        email: "Abc@email.com",
+        role: "backend engineer"
+        }
+    ])
 
-    return(
-        <form>
-            <label htmlFor ="title">Team Members</label>
-        </form>
+    return (
+        <div className="App">
+            <h1>Team Members</h1>
+            <Form addNewMember = {addNewMember} />
+        </div>
     )
 }
+
+ReactDOM.render(<App />)
