4f72fc0715a4cc00aadc92fa59cb52dd5ac51af4
Antoinette <create@aplusmedia.info>
2019-09-18 22:11:24 -0400

    created branch and App.js

---
+++ TeamBuilder/App.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react"
+
+const TeamBuilder = props => {
+
+    return(
+        <form>
+            <label htmlFor ="title">Team Members</label>
+        </form>
+    )
+}
