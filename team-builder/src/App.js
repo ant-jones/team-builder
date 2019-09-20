7e915090369695f8b404738ae53b98295c7ab3b5
Antoinette <create@aplusmedia.info>
2019-09-20 00:12:52 -0400

    completed project

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,19 +1,38 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ReactDOM from "react-dom";
+import Members from "./Members"
 import Form from "./Form"
 
 
-
-function App() {
+function  App() {
  
+ const [members, setMembers] = useState([{
+      
+  id: 1,
+  name: "Sheri",
+  email: "sheri@emai.com",
+  role: "Front-End"
+},
+
+{
+  id:2,
+  name: "Kevin",
+  email: "kevin@email.com",
+  role: "UI Dev"
+
+}]);
+const addNewMember = member => {
+  setMembers([...members, member])
+}
   return(
-    <Form />
+    <div className="App">
+    <Members membersProps={members}/>
+    <Form addNewMemberProps={addNewMember}/>
+    </div>
   ); 
    
-  
 }
 
+
 export default App;
-ReactDOM.render(<App />);
+
--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,25 +1,43 @@
-import React from "react";
-
-import teamMembersStub from "./fakerData";
-
-
-const fakeData = teamMembersStub(2);
+import React, {useState} from "react"
 
 const Form = props => {
-    const [member, setMember] = useState(fakeData);
-    const handleChanges = e => {
-        console.log(member)
-        setMember()
+    const [newMember, setNewMember] = useState({})
+
+    const handleChange = event => {
+        setNewMember({...newMember,[event.target.name]:event.target.value})
     }
+    console.log(props)
 
-
+    const handleSubmit = event => {
+        event.preventDefault();
+        const addMember = props.addNewMemberProps;
+        addMember(newMember);
+    }
     return(
-        <input
-        name = "title"
-        email = "text"
-        role = "text"
-        onChange = {handleChanges}
-        />
-    );
+        <form onSubmit = {handleSubmit}>
+            <label>
+                Name:
+                <input type="text"
+                placeholder="Name"
+                name="name"
+                onChange={handleChange}/>            
+            </label>
+            <label>
+                Email:
+                <input type="email"
+                placeholder="Email"
+                name="email"
+                onChange={handleChange}/>
+            </label>
+            <lable>
+                Role:
+                <input type="text"
+                placeholder="Role"
+                name="role"
+                onChange={handleChange}/>
+            </lable>
+            <button type="submit">Submit</button>
+        </form>
+    )
 }
 export default Form;
